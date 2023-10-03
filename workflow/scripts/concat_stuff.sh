4d5c506dd6766ffbd0f0224856f9449a01f2ce61
Wolfram Hoeps <wolfram.hoeps@embl.de>
2023-10-03 11:15:54 +0200

    Updated concat_stuff

--- workflow/scripts/concat_stuff.sh
+++ workflow/scripts/concat_stuff.sh
@@ -1,73 +1,75 @@
 #!/bin/bash
+set -euo pipefail
+export TMPDIR=$(pwd)
 
 # Check the number of arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <FileA.bcf> <FileB.vcf.gz> <output.vcf.gz>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <header.txt> <FileA.bcf> <FileB.vcf.gz> <output.vcf.gz>"
+    echo "header.txt should contain the VCF header to be applied to the merged file. Use provided template as reference."
     exit 1
 fi
 
 # Assign arguments to variables for easier reference
-FILE_A="$1"
-FILE_B="$2"
-OUTPUT="$3"
+HEADER="$1"
+FILE_A="$2"
+FILE_B="$3"
+OUTPUT="$4"
 
-# Check if input files have index
-if [ ! -f "${FILE_A}.csi" ] && [ ! -f "${FILE_A}.csi" ]; then
-    echo "Error: Index not found for $FILE_A"
-    exit 1
-fi
-
+# Check if necessary files exist
+for file in "$HEADER" "$FILE_A" "$FILE_B"; do
+    if [ ! -f "$file" ]; then
+        echo "Error: File $file does not exist."
+        exit 1
+    fi
+done
 
 # Extract sample names
-echo "Extracting sample names..."
+echo "Extracting sample names from both input files..."
 bcftools query -l "$FILE_A" > samples_A.txt
 bcftools query -l "$FILE_B" > samples_B.txt
 
 # Generate sample name mapping
 echo "Generating sample name mapping..."
-awk '
-    BEGIN {
+awk 'BEGIN {
         while (getline < "samples_A.txt") {
             original = $1;
             gsub(/^GM/, "NA", $1);
-            if ($1 != original) {
-                print original "\t" $1;
-            }
-            gsub(/^NA/, "GM", original);
-            if ($1 != original) {
-                print original "\t" $1;
-            }
+            print original "\t" $1;
         }
     }
 ' > sample_name_mapping.txt
 
 # Rename samples in File A
-echo "Renaming samples in $FILE_A..."
-bcftools reheader -s sample_name_mapping.txt "$FILE_A" > NW_flipped_50000_renamed.bcf
+echo "Renaming samples in $FILE_A based on mapping..."
+bcftools reheader -s sample_name_mapping.txt "$FILE_A" > temp_renamed.bcf
 
-# Extract renamed sample names and find common samples
-echo "Identifying common samples..."
-bcftools query -l NW_flipped_50000_renamed.bcf > samples_A_renamed.txt
+# Identify common samples
+echo "Identifying common samples between the two files..."
+bcftools query -l temp_renamed.bcf > samples_A_renamed.txt
 awk 'NR==FNR{samples[$1]=1; next} ($1 in samples)' samples_A_renamed.txt samples_B.txt > matching_samples.txt
 
 # Isolate relevant samples
 echo "Isolating relevant samples..."
-bcftools view -S matching_samples.txt NW_flipped_50000_renamed.bcf -O b > A_restricted.bcf
-bcftools view -S matching_samples.txt "$FILE_B" -O b > B_restricted.bcf
+bcftools view -S matching_samples.txt temp_renamed.bcf -O b > temp_A_restricted.bcf
+bcftools view -S matching_samples.txt "$FILE_B" -O b > temp_B_restricted.bcf
 
 # Index the BCF files
 echo "Indexing..."
-tabix -p bcf A_restricted.bcf
-sleep 5
-tabix -p bcf B_restricted.bcf
+tabix -p bcf temp_A_restricted.bcf
+sleep 1
+tabix -p bcf temp_B_restricted.bcf
 
 # Concatenate the BCF files
 echo "Concatenating files..."
-bcftools concat A_restricted.bcf B_restricted.bcf -O z -a -o "$OUTPUT"
+bcftools concat temp_A_restricted.bcf temp_B_restricted.bcf -O z -a > temp_unsorted.vcf.gz
+
+# Apply new header and sort the result
+echo "Applying the new header and sorting the merged VCF..."
+cat "$HEADER" <(grep -v '^##' <(zcat temp_unsorted.vcf.gz)) | bcftools sort --temp-dir $(pwd) -O z -o "$OUTPUT" -
 
 # Cleanup intermediate files
-echo "Cleaning up..."
-rm samples_A.txt samples_B.txt sample_name_mapping.txt NW_flipped_50000_renamed.bcf samples_A_renamed.txt matching_samples.txt A_restricted.bcf A_restricted.bcf.csi B_restricted.bcf B_restricted.bcf.csi
+echo "Cleaning up intermediate files..."
+rm -f bcftools-sort.*
+rm samples_A.txt samples_B.txt sample_name_mapping.txt temp_renamed.bcf samples_A_renamed.txt matching_samples.txt temp_A_restricted.bcf temp_B_restricted.bcf temp_unsorted.vcf.gz
 
 echo "All done! Merged file is saved as $OUTPUT."
-
